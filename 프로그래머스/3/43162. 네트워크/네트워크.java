a3121f51e19700eef6bb317bf1dd8d384b1a6e04
JIYUN <72537762+Jiyun-Parkk@users.noreply.github.com>
2024-01-28 18:13:46 +0900

    [level 3] Title: 네트워크, Time: 0.10 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public boolean[] isVisited;
+    public int network;
+    public int solution(int n, int[][] computers) {
+        isVisited = new boolean[n];
+        for(int i = 0; i < n; i++) {
+            if(!isVisited[i]) {
+                network += dfs(i,n,computers);
+             }
+          }
+        
+        return network;
+    }
+    public int dfs(int i, int n, int[][] computers) {
+        if(isVisited[i]){
+            return 0;
+        }
+        
+        isVisited[i] = true;
+        for(int j = 0; j < n; j++) {
+            if(computers[i][j] == 1) {
+                dfs(j,n,computers);
+            }
+        }
+        return 1;
+    }
+}
